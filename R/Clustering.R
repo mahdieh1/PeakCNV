3a0c81c4586b9c5fe605d2122ee1e6c915ae642b
MahdiehLabani <mahdieh.labani@gmail.com>
2021-09-14 09:46:42 +0430

    Update Clustering.R

--- R/Clustering.R
+++ R/Clustering.R
@@ -2,13 +2,14 @@
 Clustering<-function()
 {
   Chr.cluster<-data.frame();
-  win<-read.table("overlap_Bin_Case.bed")#,header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
-  #win$V2=win$V2-2;#remove it puts in building cnvrs
+  win<-read.table("overlap_del_case.bed")#,header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
+ # win$V2=win$V2-2;#remove it puts in building cnvrs
   #win<-win[,c(1,2,3,6)]
   f<-unique(win$V1)
-
+  
   for(k in f)
-  {
+  
+  { 
     total<-data.frame();
     df<-data.frame();
     row<-data.frame();
@@ -24,7 +25,7 @@ Clustering<-function()
         df[i,j]<- abs(col[i,1]-row[1,j])
       }
     }
-
+    
     subtract<-read.table(file=paste(k, "subtract.txt", sep=""),header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
     total<-cbind(df,subtract)
     if(nrow(total)<5){
@@ -39,10 +40,10 @@ Clustering<-function()
     cat("Enter value for eps : \n")
     threshold <- as.integer(readline(prompt = ""))
     abline(h=threshold, col="red")
-
+    
     db = dbscan(iris_matrix, threshold, rate)
-    c<-db$cluster
-    c<-data.frame(win_sel,c)
+    c<-db$cluster 
+    c<-data.frame(win_sel,c) 
     write.table(c, file=paste(k, "DBSCAN.txt", sep=""), sep="\t", col.names = F, row.names = F)
     Chr.cluster<-rbind(Chr.cluster,c)
   }
