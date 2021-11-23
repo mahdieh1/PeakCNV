5878492461ab9dab0945c26127fb14d4d86e4fc3
MahdiehLabani <mahdieh.labani@gmail.com>
2021-11-24 00:35:45 +0330

    Update BuildingCNVRs.R

--- R/BuildingCNVRs.R
+++ R/BuildingCNVRs.R
@@ -47,11 +47,11 @@ BuildingCNVRs<- function()
   
   
   #Apply filter threshold
-  print('The minimum pvalue for your data is:')  
-  print(min(new.df$V8))
-  cat("Enter thrshold for p-value : \n")
-  threshold <- as.integer(readline(prompt = ""))
-  #threshold=0.05;
+  #print('The minimum pvalue for your data is:')  
+  #print(min(new.df$V8))
+  #cat("Enter thrshold for p-value : \n")
+  #threshold <- as.integer(readline(prompt = ""))
+  threshold=0.05;
   CNVRs <- new.df[new.df[,8]< threshold,]
   CNVRs<-CNVRs[,c(1,2,3)]
   CNVRs<-CNVRs[!duplicated(CNVRs), ]
